5dae5d91b96e48c08c38f97ad8a9b22c42d61031
wyz135 <30320784+wyz135@users.noreply.github.com>
2018-03-03 21:47:39 +1100

    Update logistic_map.py

--- LogisticMap/logistic_map.py
+++ LogisticMap/logistic_map.py
@@ -2,17 +2,23 @@
 
 import matplotlib.pyplot as plt
 
+# Initial population and mu value
 x = 0.75
 mu = 4.*(1-2E-14)
-xi = []
+
 n = 200
 N = range(0,n+1)
 
+# Obtain the population value over n iterations
+xi = []
 for n in N:
     x = mu*x*(1-x)
     xi.append(x)
-    
+ 
+# Plotting the evolution of population   
 plt.plot(N,xi)
 plt.ylim([0,1])
+plt.ylabel("Population x")
 plt.xlim([0,n])
+plt.xlabel("Time, n")
 plt.show()
